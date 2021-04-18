a424de00345f72153ce1cac0bfa5d049a9af14fe
Drew Thomas <amthomas12@bsu.edu>
2021-04-17 21:43:15 -0400

    Remove failing test

--- server/tests/EnvironmentHandler.test.ts
+++ server/tests/EnvironmentHandler.test.ts
@@ -9,8 +9,5 @@ describe('EnvironmentHandler', function() {
             expect(process.env['KEY']).to.be.equal('VALUE');
             expect(process.env['KEY_2']).to.be.equal('VALUE_2');
         });
-        it('should fail', () => {
-            expect(2).to.be.equal(3);
-        })
     });
 });
