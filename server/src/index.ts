6bf1072cf7a523baba7070a702cc678a2075d809
Drew Thomas <41974547+DrewThomasCorps@users.noreply.github.com>
2021-04-18 16:22:07 -0400

    Add more set up (#3)
    * Set up minimal backend
    
    * Add environment handler and tests
    
    * Create server-ci-tests.yml
    
    * Update server-ci-tests.yml
    
    * Add failing test to test workflow
    
    * Remove failing test
    
    * Add vessel model

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,20 @@
 import {IncomingMessage, ServerResponse} from "http";
+import {MongoClient} from "mongodb";
+import EnvironmentHandler from "./EnvironmentHandler";
 
-const MongoClient = require('mongodb').MongoClient;
 const dbName = 'ais_project';
 
 const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+
+const environmentHandler = new EnvironmentHandler(".env");
+
 const server = http.createServer((_request: IncomingMessage, response: ServerResponse) => {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
 
     const client = new MongoClient('mongodb://localhost:27017', {useUnifiedTopology: true});
     try {
-        client.connect().then((client: any) => {
+        client.connect().then((client: MongoClient) => {
             const vessels = client.db(dbName).collection('vessels');
             vessels.find({IMO: 1000186}).toArray().then((vessels: any) => {
                 response.end(`IMO: ${vessels[0].IMO} | Built: ${vessels[0].Built}`);
@@ -24,6 +26,7 @@ const server = http.createServer((_request: IncomingMessage, response: ServerRes
         // client.close();
     }
 });
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+environmentHandler.setUp();
+server.listen(process.env["NODE_PORT"], process.env["NODE_HOSTNAME"], () => {
+    console.log(`Server running at http://${process.env["NODE_PORT"]}:${process.env["NODE_HOSTNAME"]}/`);
 });
