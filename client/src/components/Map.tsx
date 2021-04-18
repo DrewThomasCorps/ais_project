7841eff59a0bf4c60289c6f00ecccc8365e25d98
Adbauer89 <39104195+Adbauer89@users.noreply.github.com>
2021-04-17 22:11:14 -0400

    Refactor Map to use svg with background image (#4)

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,36 +1,22 @@
 import React, {Fragment} from 'react';
 
-const Map = ({ currentZoom, setCurrentZoom }: {currentZoom: number, setCurrentZoom: any}) => {
-
-    const renderMapImages = () => {
-        let images = <Fragment/>
-
-        switch (currentZoom) {
-            case 1:
-                images = <Fragment>
-                    <img src={`./resources/ROOT.png`} className={`map-image`} alt={``}/>
-                </Fragment>
-                break;
-            case 2:
-                images = <Fragment>
-                    <img src={`./resources/38G0.png`} className={`map-image`} alt={``}/>
-                </Fragment>
-                break;
-            case 3:
-                images = <Fragment>
-                    <img src={`./resources/38G01.png`} className={`map-image`} alt={``}/>
-                </Fragment>
-                break;
-
-        }
-
-        return images;
-    }
+const Map = ({ currentImageName, handleClick }: {currentImageName: string, handleClick: any}) => {
 
     return (
         <Fragment>
-            <div className={`map-image-container`} onClick={setCurrentZoom}>
-                {renderMapImages()}
+            <div className={`map-image-container`} id={`map`} onClick={handleClick}>
+                { currentImageName &&
+                    <svg viewBox="0 0 100 100" className="map-image"
+                         style={{backgroundImage: `url("./resources/${currentImageName}")`, backgroundSize: `cover`}}>
+                        <circle cx="12.5" cy="6.7" r="3" strokeWidth="1" stroke="#FF000055" fill="#FF000055">
+                            <animate attributeType="SVG" attributeName="r" begin="0s" dur="2.0s" repeatCount="indefinite" from="0%" to="1%"/>
+                            <animate attributeType="CSS" attributeName="stroke-width" begin="0s"  dur="2.0s" repeatCount="indefinite" from="0%" to="4%" />
+                            <animate attributeType="CSS" attributeName="opacity" begin="0s"  dur="2.0s" repeatCount="indefinite" from="1" to="0"/>
+                        </circle>
+                    </svg>
+                }
+
+
             </div>
         </Fragment>
     )
