dc6dda16ed7d6dd99635a3cad022cd48659f8aa5
Alan Bauer <abauerdev@gmail.com>
2021-04-18 16:27:32 -0400

    Add basic routing

---
+++ server/src/controller.ts
@@ -0,0 +1,38 @@
+import {IncomingMessage, ServerResponse} from "http";
+
+const MongoClient = require('mongodb').MongoClient;
+const dbName = 'ais_project';
+
+exports.getVessels = (_request: IncomingMessage, response: ServerResponse) => {
+    const client = new MongoClient('mongodb://localhost:27017', {useUnifiedTopology: true});
+
+    try {
+        client.connect().then((client: any) => {
+            const vessels = client.db(dbName).collection('vessels');
+
+            vessels.find().toArray().then((vessels: any) => {
+                response.statusCode = 200;
+                response.setHeader('content-Type', 'Application/json');
+                response.end(JSON.stringify(vessels));
+            })
+        })
+    } finally {
+        // client.close();
+    }
+}
+
+exports.createVessel = (request: IncomingMessage, response: ServerResponse) => {
+    let body = '';
+
+    request.on('data',  (chunk: any) => {
+        body += chunk;
+    });
+
+    request.on('end', () => {
+        const responseBody = JSON.parse(body);
+
+        response.statusCode = 201;
+        response.setHeader('content-Type', 'Application/json');
+        response.end(JSON.stringify(responseBody))
+    })
+}
