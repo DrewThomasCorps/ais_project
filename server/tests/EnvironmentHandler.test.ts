cd59b168eee91178da5fe9dd93d4003212fb793c
Drew Thomas <amthomas12@bsu.edu>
2021-04-17 21:22:10 -0400

    Add environment handler and tests

---
+++ server/tests/EnvironmentHandler.test.ts
@@ -0,0 +1,13 @@
+import {expect} from 'chai';
+import EnvironmentHandler from "../src/EnvironmentHandler";
+
+describe('EnvironmentHandler', function() {
+    describe('setUp()', function() {
+        it('should set environment variables', function() {
+            const environmentHandler = new EnvironmentHandler("tests/resources/.env.test");
+            environmentHandler.setUp();
+            expect(process.env['KEY']).to.be.equal('VALUE');
+            expect(process.env['KEY_2']).to.be.equal('VALUE_2');
+        });
+    });
+});
